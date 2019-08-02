73bea3f2497640eb7a2562dfd0ad8ea9b45de546
alexandre thadeu <alexandre.thadeu@icarros.com.br>
2019-08-02 17:01:12 -0300

    Removendo Pods dos Commits

---
+++ Movies/Movies/Scene/Movies/Controllers/MovieTabBarViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BaseTabBarViewController.swift
+//  Movies
+//
+//  Created by Alexandre Thadeu on 02/08/19.
+//  Copyright © 2019 AlexandreThadeu. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        
+    }
+}
---
+++ Movies/Movies/Utils/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  Movies
+//
+//  Created by Alexandre Thadeu on 02/08/19.
+//  Copyright © 2019 AlexandreThadeu. All rights reserved.
+//
+
+import Foundation
